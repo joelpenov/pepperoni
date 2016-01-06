f03fbc24de9f0c28e456a007e6b1eeced593c93a
Jorge Ferreira <jorgeisaacferreira@gmail.com>
2016-01-06 09:11:29 -0400

    translating form to spanish

--- products/forms.py
+++ products/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 
 class ProductForm(forms.Form):
-    description = forms.CharField(max_length=255)
-    show_in_menu= forms.BooleanField()
-    is_raw_material= forms.BooleanField()
-    sell_price = forms.FloatField()
+    description = forms.CharField(max_length=255, label="Descripcion")
+    show_in_menu= forms.BooleanField(label="Mostrar en el menu")
+    is_raw_material= forms.BooleanField(label="Es materia prima")
+    sell_price = forms.FloatField("Precio de venta")
