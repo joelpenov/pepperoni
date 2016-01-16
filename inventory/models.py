ed61cb659dfc5e4d6399801bd4852dcafd2de485
Joel Pena <jukey09@gmail.com>
2016-01-16 19:55:16 -0400

    Add inventory models

--- inventory/models.py
+++ inventory/models.py
@@ -1,16 +1,20 @@
+
 from django.db import models
 from products.models import Product
 
-
-# Create your models here.
 class Warehouse(models.Model):
 	id = models.IntegerField(primary_key=True)
 	name = models.CharField(max_length=50)
 
-
 class InventoryMove(models.Model):
-	Warehouse = models.ForeignKey(Warehouse, related_name="fk_inventory_move_warehouse", null=True, default=None)
+	id = models.IntegerField(primary_key=True)
+	Warehouse = models.ForeignKey(Warehouse, related_name="fk_inventory_move_warehouse", default=None)
 	transaction_type = models.IntegerField()
-	quantity_type = models.FloatField()
-	price = models.FloatField()
 
+class InventoryMoveDetail(models.Model):
+	id = models.IntegerField(primary_key=True)
+	InventoryMove = models.ForeignKey(InventoryMove, related_name="fk_inventory_move_detail_inventory_move", default=None)
+	Product = models.ForeignKey(Product, related_name="fk_inventory_move_detail_product", default=None)
+	quantity = models.FloatField()
+	cost = models.FloatField()
+	price = models.FloatField()
