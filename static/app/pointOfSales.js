75f79449f1d9c4ce894f95bbc5fc03d04cacf411
Jorge Ferreira <jorgeisaacferreira@gmail.com>
2016-01-25 18:05:55 -0400

    POS

--- static/app/pointOfSales.js
+++ static/app/pointOfSales.js
@@ -2,6 +2,67 @@
     function PointOfSalesView(){
         var self = this;
         self.menuItems = ko.observableArray();
+        self.productIdHasError = ko.observable(false);
+        self.quantityHasError = ko.observable(false);
+        self.priceHasError = ko.observable(false);
+        self.productId = ko.observable();
+        self.productDescription = ko.observable();
+        self.quantity = ko.observable();
+        self.price = ko.observable();
+        self.orderDetails= ko.observableArray();
+
+        self.productId.subscribe(function () {
+            if(!self.productId()){
+                self.productDescription("")
+                return;
+            }
+
+            GenericViews.getDataById("/api/products/", self.productId(),function (response) {
+                self.productDescription(response.description)
+            });
+        });
+
+        function isNumber(value) {
+
+            var er = /^[0-9]|[0-9](.([0-9]))+$/;
+
+            return er.test(value);
+        }
+
+        self.cleanDetails = function(){
+            self.productId(null);
+            self.productDescription(null);
+            self.quantity(null);
+            self.price(null);
+
+            self.productIdHasError(false);
+            self.quantityHasError(false);
+            self.priceHasError(false);
+        };
+
+        self.addNewProduct = function () {
+
+            self.productIdHasError(!isNumber(self.productId()) || !self.productDescription());
+            self.quantityHasError(!isNumber(self.quantity()));
+            self.priceHasError(!isNumber(self.price()));
+
+            if(self.productIdHasError() || self.quantityHasError() || self.priceHasError()) return;
+
+            self.orderDetails.push({
+                product_id:self.productId(),
+                product_description: self.productDescription(),
+                quantity: self.quantity(),
+                price: self.price(),
+                total: (self.quantity() * self.price())
+            });
+
+            self.cleanDetails();
+        };
+
+        self.deleteProduct = function(product){
+            self.orderDetails.remove(product);
+        };
+
     }
 
     $(document).ready(function(){
