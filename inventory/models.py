1abd144ef1a9fcc7c7f51362839197ca207855a9
Joel Peña Arias <jpena@intellisys.com.do>
2016-02-04 20:42:28 -0400

    Update stock on input/output transaction

--- inventory/models.py
+++ inventory/models.py
@@ -47,38 +47,22 @@ class InventoryMoveDetail(models.Model):
 	price = models.FloatField()
 	total = models.FloatField()
 
-#attempt to update the stock, not working need to rethink this
-	# def updateStock(self):
-	# 	move =self.inventory_move;
-	# 	stock = Stock.objects.filter(product_id=self.product_id).filter(warehouse_id=move.warehouse_id).first()
-	# 	if(stock==None):
-	# 		stock = Stock.objects.create(product_id=self.product_id, warehouse_id  = move.warehouse_id, quantity = 0)
-    #
-	# 	if(move.transaction_type==InventoryMove.INPUT):
-	# 		stock.quantity = stock.quantity + self.quantity
-	# 	else:
-	# 		stock.quantity = stock.quantity - self.quantity
-    #
-	# 	stock.save()
-    #
-    #
-	# def save(self, *args, **kwargs):
-	# 	super(InventoryMoveDetail, self).save(*args, **kwargs)
-	# 	self.updateStock()
-    #
-	# def delete(self, *args, **kwargs):
-	# 	super(InventoryMoveDetail, self).delete(*args, **kwargs)
-    #
-	# 	move =self.inventory_move;
-	# 	stock = Stock.objects.filter(product_id=self.product_id).filter(warehouse_id=move.warehouse_id).first()
-	# 	if(stock==None):
-	# 		stock = Stock.objects.create(product_id=self.product_id, warehouse_id  = move.warehouse_id, quantity = 0)
-    #
-	# 	if(move.transaction_type==InventoryMove.OUTPUT):
-	# 		stock.quantity = stock.quantity + self.quantity
-	# 	else:
-	# 		stock.quantity = stock.quantity - self.quantity
-    #
-	# 	stock.save()
 
-
+	def updateStock(self):
+		move =self.inventory_move;
+		stock = Stock.objects.filter(product_id=self.product_id).filter(warehouse_id=move.warehouse_id).first()
+		if(stock==None):
+			stock = Stock.objects.create(product_id=self.product_id, warehouse_id  = move.warehouse_id, quantity = 0)
+    
+		if(move.transaction_type==InventoryMove.INPUT):
+			stock.quantity = stock.quantity + self.quantity
+		else:
+			stock.quantity = stock.quantity - self.quantity
+    
+		stock.save()
+    
+    
+	def save(self, *args, **kwargs):
+		super(InventoryMoveDetail, self).save(*args, **kwargs)
+		self.updateStock()
+    
