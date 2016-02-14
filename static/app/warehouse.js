d360d34f7e2bc3f2d86d1750c9b32a561288816c
Joel Peña Arias <jpena@intellisys.com.do>
2016-02-13 20:57:50 -0400

    Set datatable language properties to Spanish

--- static/app/warehouse.js
+++ static/app/warehouse.js
@@ -4,7 +4,31 @@
             url: "/api/warehouses/",
             dataTable: $('#dynamic-table').dataTable({
                 //"aoColumns": columns,
-                data: []
+                data: [],
+                language:{
+                            "sProcessing":     "Procesando...",
+                            "sLengthMenu":     "Mostrar _MENU_ registros",
+                            "sZeroRecords":    "No se encontraron resultados",
+                            "sEmptyTable":     "Ningún dato disponible en esta tabla",
+                            "sInfo":           "Mostrando del _START_ al _END_ de un total de _TOTAL_ registros",
+                            "sInfoEmpty":      "Mostrando del 0 al 0 de un total de 0 registros",
+                            "sInfoFiltered":   "(filtrados de un total de _MAX_ registros)",
+                            "sInfoPostFix":    "",
+                            "sSearch":         "Buscar:",
+                            "sUrl":            "",
+                            "sInfoThousands":  ",",
+                            "sLoadingRecords": "Cargando...",
+                            "oPaginate": {
+                                "sFirst":    "Primero",
+                                "sLast":     "Último",
+                                "sNext":     "Siguiente",
+                                "sPrevious": "Anterior"
+                            },
+                            "oAria": {
+                                "sSortAscending":  ": Ordenar la columna de manera ascendente",
+                                "sSortDescending": ": Ordenar la columna de manera descendente"
+                            }
+                        }
             })
         };
 
