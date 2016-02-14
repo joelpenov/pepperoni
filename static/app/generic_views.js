607756a11a1ee644f10b467f9afb965254a2386d
Joel Peña Arias <jpena@intellisys.com.do>
2016-02-13 21:22:08 -0400

    Add utils.js file and move datatable language to it

--- static/app/generic_views.js
+++ static/app/generic_views.js
@@ -177,6 +177,31 @@ var GenericViews = GenericViews || {};
 
     function DataTableView(settings) {
         self = this;
+        settings.language =
+                           [ {
+                            "sProcessing":     "Procesando...",
+                            "sLengthMenu":     "Mostrar _MENU_ registros",
+                            "sZeroRecords":    "No se encontraron resultados",
+                            "sEmptyTable":     "Ningún dato disponible en esta tabla",
+                            "sInfo":           "Mostrando del _START_ al _END_ de un total de _TOTAL_ registros",
+                            "sInfoEmpty":      "Mostrando del 0 al 0 de un total de 0 registros",
+                            "sInfoFiltered":   "(filtrados de un total de _MAX_ registros)",
+                            "sInfoPostFix":    "",
+                            "sSearch":         "Buscar:",
+                            "sUrl":            "",
+                            "sInfoThousands":  ",",
+                            "sLoadingRecords": "Cargando...",
+                            "oPaginate": {
+                                "sFirst":    "Primero",
+                                "sLast":     "Último",
+                                "sNext":     "Siguiente",
+                                "sPrevious": "Anterior"
+                            },
+                            "oAria": {
+                                "sSortAscending":  ": Ordenar la columna de manera ascendente",
+                                "sSortDescending": ": Ordenar la columna de manera descendente"
+                            }
+                        }];
         self.dataTable= settings.dataTable;
         settings.actionRender = settings.actionRender || function(item){
                 return '<div class="action-buttons"><a class="edit green" data-item-id="' + item.id + '"><i class="ace-icon fa fa-pencil bigger-130"></i></a></div>';
