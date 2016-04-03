84e5fe999a7aa65e012e5d3ea825abb0c5b99809
Jorge Ferreira <jorgeisaacferreira@gmail.com>
2016-04-03 12:27:24 -0400

    show cashier shift history

--- static/app/cashierShift.js
+++ static/app/cashierShift.js
@@ -2,7 +2,157 @@ var PEPPERONI = PEPPERONI || {};
 
 (function(){
 
+    function ShiftListView(){
+        var self = this;
+        self.showShiftList=ko.observable(true);
+
+        self.show=function(){
+            self.showShiftList(true);
+        };
+
+        self.hide=function(){
+            self.showShiftList(false);
+        };
+
+
+        self.onShow =function(callback){
+            self.showShiftList.subscribe(function(value){
+                if(value) callback();
+            });
+        };
+
+        self.onHide =function(callback){
+            self.showShiftList.subscribe(function(value){
+                if(!value) callback();
+            });
+        };
+
+        self.init=function(){
+            ko.applyBindings(self,document.getElementById('cashier-shift-view'));
+        };
+
+    }
+
+    function FinishShiftView(settings){
+        var self = this;
+        self.showFinishSwiftView = ko.observable(false);
+        self.cashierShift = ko.observable({});
+
+        self.totalRegister = ko.observable(0).extend({numeric:2});
+        self.totalActive = ko.observable(0).extend({numeric:2});
+        self.totalFinished = ko.observable(0).extend({numeric:2});
+        self.totalVoid = ko.observable(0).extend({numeric:2});
+        self.difference = ko.observable(0).extend({numeric:2});
+        self.totalNotDelivered = ko.observable(0).extend({numeric:2});
+        self.orders = ko.observableArray();
+        self.activeOrders = ko.observableArray();
+        self.finishedOrders = ko.observableArray();
+        self.voidOrders = ko.observableArray();
+
+        self.startBalance = ko.computed(function(){
+            return self.cashierShift().start_balance;
+        });
+
+        self.totalRegister.subscribe(function(){
+            self.difference(self.totalRegister()-self.startBalance()-self.totalFinished());
+        });
+
+        self.filterBy=function(matches){
+            var total = 0;
+            var filterOrders= [];
+            self.orders().forEach(function(order){
+                if(matches(order)) {
+                    total += order.total;
+                    filterOrders.push(order);
+                }
+            });
+            return {
+                total:total,
+                orders:filterOrders
+            };
+        };
+
+        self.orders.subscribe(function(){
+            var notDeliveredOrderFilter = self.filterBy(function(order){return order.status ==='FINISHED' && order.delivered!==true});
+            self.totalNotDelivered(notDeliveredOrderFilter.total);
+            self.difference(self.totalRegister()-self.totalActive());
+
+            var activesOrderFilter = self.filterBy(function(order){return order.status ==='ACTIVE'});
+            self.activeOrders(activesOrderFilter.orders);
+            self.totalActive(activesOrderFilter.total);
+
+            var finishedOrderFilter = self.filterBy(function(order){return order.status ==='FINISHED'});
+            self.finishedOrders(finishedOrderFilter.orders);
+            self.totalFinished(finishedOrderFilter.total);
+
+            var voidOrderFilter = self.filterBy(function(order){return order.status ==='VOID'});
+            self.voidOrders(voidOrderFilter.orders);
+            self.totalVoid(voidOrderFilter.total);
+        });
+
+        self.updateLayout = function(){
+            var orderReviewContainer = $('.order-review');
+            orderReviewContainer.height(innerHeight- orderReviewContainer.offset().top);
+        };
+
+
+        $(window).resize(function(){
+            if(self.showFinishSwiftView()){
+                self.updateLayout();
+            }
+        });
+
+
+        self.show=function(){
+            self.showFinishSwiftView(true);
+            self.updateLayout();
+        };
+
+        self.hide=function(){
+            self.showFinishSwiftView(false);
+        };
+
+
+        self.onShow =function(callback){
+            self.showFinishSwiftView.subscribe(function(value){
+                if(value) callback();
+            });
+        };
+
+        self.onHide =function(callback){
+            self.showFinishSwiftView.subscribe(function(value){
+                if(!value) callback();
+            });
+        };
+
+        self.toggleDetails=function(order){
+            order.showDetails(!order.showDetails());
+        };
+
+        self.cancel = function(){
+            self.showFinishSwiftView(false);
+        };
+
+        self.init=function(){
+            ko.applyBindings(self,document.getElementById('finish-shift-view'));
+        };
+    }
+
+    function initializeFinishShiftView(){
+        var finishShiftView = new FinishShiftView({});
+        finishShiftView.init();
+        return finishShiftView;
+    }
+    function initializeShiftListView(){
+        var shiftListView = new ShiftListView({});
+        shiftListView.init();
+        return shiftListView;
+    }
+
     $(document).ready(function(){
+
+        var shiftView = initializeFinishShiftView();
+        var shiftListView = initializeShiftListView();
         var table_settings = {
             url: "/api/cashiershifts/",
             dataTable: PEPPERONI.createDatatableInstance({tableId: '#dynamic-table'})
@@ -11,5 +161,31 @@ var PEPPERONI = PEPPERONI || {};
         var dataTableView = new GenericViews.DataTableView(table_settings);
 
         dataTableView.refreshDataTable();
+
+        shiftListView.onShow(function(){
+            shiftView.hide();
+        });
+
+        shiftListView.onHide(function(){
+            shiftView.show();
+        })
+
+        shiftView.onHide(function(){
+            shiftListView.show();
+        });
+
+        table_settings.dataTable.on('click', '.action-buttons .view', function(){
+            var id = $(this).data("item-id");
+            GenericViews.getData("/api/cashiershifts/"+id+"?format=json", function(response){
+                shiftView.cashierShift(response);
+                GenericViews.getData("/api/orders/?format=json&cashier_shift="+response.id, function(response){
+                    response.forEach(function(order){
+                        order.showDetails = ko.observable(false);
+                    });
+                    shiftView.orders(response);
+                    shiftListView.hide();
+                });
+            });
+        });
     });
 })();
