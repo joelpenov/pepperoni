ab0fcc44712941d3f18c2bf149f7f63507741bd9
Joel Peña Arias <jukey09@gmail.com>
2016-05-08 16:41:10 -0400

    Display menu by permissions

--- sales/views/Customer.py
+++ sales/views/Customer.py
@@ -4,10 +4,8 @@ from rest_framework import viewsets, permissions, filters
 from sales.models.Customer import Customer
 from sales.serializers.Customer import CustomerSerializer
 from main.mixin import AtomicMixin
-from django.contrib.auth.decorators import permission_required
 
 @login_required()
-@permission_required('add_warehouse')
 def customers(request):
     return render(request,"sales/customer.html")
 
