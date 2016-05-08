32b1b6c27e1abe4deacea3b96f4e41ca31babeb6
Joel Peña Arias <jukey09@gmail.com>
2016-05-08 15:07:42 -0400

    Add permission implementation

--- inventory/views/Stock.py
+++ inventory/views/Stock.py
@@ -4,9 +4,12 @@ from rest_framework import viewsets, permissions, filters
 from inventory.models.Stock import Stock
 from inventory.serializers.Stock import StockSerializer
 from main.mixin import AtomicMixin
+from django.contrib.auth.decorators import permission_required
 
 
+# admin_user
 @login_required()
+@permission_required('add_warehouse')
 def stock(request):
 	return render(request,"inventory/InventoryStock.html")
 
