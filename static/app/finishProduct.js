a61f83b290fdccc2a1d0de0b37858554e058cfbb
Jorge Ferreira <jorgeisaacferreira@gmail.com>
2016-05-16 06:39:11 -0400

    showing row material list

--- static/app/finishProduct.js
+++ static/app/finishProduct.js
@@ -1,13 +1,36 @@
 (function(){
     function ProductUsageModel() {
         var factory= this;
-
-
+        factory.included = ko.observable(true);
+        factory.itemId = ko.observable();
+        factory.itemName = ko.observable();
+        factory.setData = function(data){
+            factory.itemId (data.id);
+            factory.itemName (data.description);
+            return factory;
+        };
     }
 
     function FinishProductionView(){
         var factory = this;
-        factory.ProductUsages = ko.observableArray();
+        factory.productUsages = ko.observableArray();
+
+
+        factory.init = function () {
+            ko.applyBindings(factory, document.getElementById('product-usages-view'));
+        };
     }
 
+
+    $(document).ready(function(){
+        var finishProductView = new FinishProductionView();
+        finishProductView.init();
+        GenericViews.getData("/api/products/?format=json&is_raw_material=True", function(response){
+            var tempItems = [];
+            response.forEach(function(data){
+                tempItems.push(new ProductUsageModel().setData(data));
+            });
+            finishProductView.productUsages(tempItems);
+        });
+    });
 })();
