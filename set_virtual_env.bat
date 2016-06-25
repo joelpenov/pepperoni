b9ed035ce43705cdeb907215e4986275455607f0
Crunchy Server Windows Terminal 1 <juancrunchy@gmail.com>
2016-06-24 20:21:20 -0400

    Change requirements order in order to fix msql issue

--- set_virtual_env.bat
+++ set_virtual_env.bat
@@ -1,4 +1,4 @@
-virtualenv crunchyenv -p c:\python34\python.exe &
-pip install libs\mysqlclient-1.3.7-cp34-none-win32.whl &
+virtualenv crunchyenv -p c:\python34\python.exe &  crunchyenv\scripts\activate &
+pip install libs\mysqlclient-1.3.4-cp34-none-win_amd64.whl &
 pip install -r requirements.txt &
-python -m pip install --upgrade pip & crunchyenv\scripts\activate &
+python -m pip install --upgrade pip
