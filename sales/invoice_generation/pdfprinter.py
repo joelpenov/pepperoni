1d4687c7e2c7f0c4b05c375c6600586a1ea858df
Crunchy Server Windows Terminal 1 <juancrunchy@gmail.com>
2016-06-25 21:55:02 -0400

    Set printer name network

--- sales/invoice_generation/pdfprinter.py
+++ sales/invoice_generation/pdfprinter.py
@@ -1,10 +1,18 @@
 import os, sys
 #import cups
+import tempfile
+import win32api
+import win32print
+
 
 def print_pdf(filepath):
-	os.startfile(filepath, "print")
+	print(filepath)
+	filepath = filepath.replace( r'\\', r'\\\\' )
+	#os.startfile(filepath, "print")
 	#if sys.platform == "win32":
 	#	os.startfile(filepath, "print")
 	#else:
 	#	conn = cups.Connection()
 	#	conn.printFile('Epson-TM-BA-Thermal', filepath, 'Pepperoni', {})
+	#win32print.GetDefaultPrinter()
+	win32api.ShellExecute(0,"print", filepath, '"%s"' % r"\\\\DESKTOP-0H0DJC5\\StarTSP700", ".", 0)
